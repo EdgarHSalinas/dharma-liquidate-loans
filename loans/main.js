ea5730d088e7376ad13514a6ce20bf06aeb6ed11
Your Name <EdgarHSalinas@gmail.com>
2019-06-07 13:19:12 -0700

    take home test complete

--- loans/main.js
+++ loans/main.js
@@ -1,7 +1,99 @@
+/**
+ *
+ *  Assumptions for time constrains
+ *
+ * if (principalTokenId: null) = USDC = DAI all equal to $1
+ *
+ *  Token was not needed for calculation since they were both equal
+ *
+ *  Took the Minimum price for the day if collateral was less then it was added to output Array.
+ *
+ *  Todo: Refactor
+ *  for loops into Array built in methods
+ *
+ *
+ */
+
+const loans = require('../loans.json');
+const etherPrice = require('../eth-price.json');
+const tokens = require('../tokens.json');
+
+const MS_PER_DAY = 1000 * 60 * 60 * 24; // 86400000
+
 // Given a date, returns which loans were liquidatable.
-function getLiquidatableLoans(givenDate) {
-	// STUB.
-	return [1, 2, 3];
+
+async function getLiquidatableLoans(givenDate) {
+  const etherMinPriceForGivenDate = await getMinEtherPricesForGivenDate(
+    givenDate,
+    etherPrice
+  );
+
+  const loansIDToLiquidate = await getLoansToLiquidateForDay(
+    givenDate,
+    loans,
+    etherMinPriceForGivenDate
+  );
+
+  return [...loansIDToLiquidate];
 }
 
-module.exports = getLiquidatableLoans;
+function getMinEtherPricesForGivenDate(givenDate, etherData) {
+  const etherPricesForDate = [];
+  const dateToFilter = givenDate.getDate();
+
+  for (let i = 0; i < etherData.length; i++) {
+    let ether = etherData[i];
+
+    let dateOfEther = new Date(ether.date).getDate();
+    if (dateToFilter === dateOfEther) {
+      etherPricesForDate.push(ether.value);
+    }
+  }
+  return Math.min(...etherPricesForDate);
+}
+
+function getLoansToLiquidateForDay(date, loans, minEtherPrice) {
+  const loansID = [];
+
+  for (let j = 0; j < loans.length; j++) {
+    let loan = loans[j];
+
+    let collateral = loan.collateralAmount * minEtherPrice;
+
+    let principalAmount = loan.principalAmount;
+
+    if (isLoanPastDue(date, loan)) {
+      loansID.push(loan.id);
+    } else if (collateral === 0) {
+      loansID.push(loan.id);
+    } else {
+      if (collateral < principalAmount) {
+        loansID.push(loan.id);
+      }
+    }
+  }
+  return loansID;
+}
+
+function isLoanPastDue(dateToTest, loan) {
+  const dateToTestInMS = dateToTest.getTime();
+
+  const loanIssueDate = parseInt(loan.filledAt);
+  const daysToExpire = loan.durationInDays;
+  const durationOfLoanInMS = MS_PER_DAY * daysToExpire;
+
+  const loanExpire = loanIssueDate + durationOfLoanInMS;
+
+  if (dateToTestInMS > loanExpire) {
+    return true;
+  }
+  return false;
+}
+
+module.exports = {
+  getLiquidatableLoans,
+  getMinEtherPricesForGivenDate,
+  getLoansToLiquidateForDay,
+  isLoanPastDue,
+  MS_PER_DAY
+};
--- loans/main.spec.js
+++ loans/main.spec.js
@@ -1,18 +1,97 @@
 // Libraries
-const chai = require("chai");
+const chai = require('chai');
 // Function to test
-const getLiquidatableLoans = require("./main");
+const {
+  getLiquidatableLoans,
+  getMinEtherPricesForGivenDate,
+  getLoansToLiquidateForDay,
+  isLoanPastDue,
+  MS_PER_DAY
+} = require('./main');
+
+const loansTest = require('./loans-test.json');
+const etherTestPrice = require('./eth-test-price');
 
 const expect = chai.expect;
+const dateToTest = new Date(1558650159442);
 
-describe("getLiquidatableLoans", () => {
-	describe("when given the current date", () => {
-		it("returns an array with 1, 2 and 3", async () => {
-			const dateToTest = new Date();
-			
-			const result = getLiquidatableLoans(dateToTest);
+describe('getMinEtherPricesForGivenDate', () => {
+  describe('when given the current date', () => {
+    it('returns the minimum price for the day ', async () => {
+      const minPrice = getMinEtherPricesForGivenDate(
+        dateToTest,
+        etherTestPrice
+      );
 
-			expect(result).to.eql([1, 2, 3]);
-		});
-	});
+      expect(minPrice).to.eql(244.9773);
+    });
+  });
+});
+
+describe('getLoansToLiquidateForDay', () => {
+  describe('when given the current date', () => {
+    it('returns an array with IDs', async () => {
+      const minPrice = 244.9773;
+
+      const result = getLoansToLiquidateForDay(dateToTest, loansTest, minPrice);
+
+      expect(result).to.eql([6368, 6367, 5877]);
+    });
+  });
+});
+
+const testLoan = {
+  id: 6370,
+  principalAmount: 0.1388,
+  collateralAmount: 0.001,
+  principalTokenId: 4,
+  collateralTokenId: 1,
+  filledAt: '1558650159442',
+  durationInDays: 90
+};
+
+describe('isLoanPastDue', () => {
+  describe('when given the current date', () => {
+    it('returns false if loan is not expired ', async () => {
+      const result = isLoanPastDue(dateToTest, testLoan);
+
+      expect(result).to.eql(false);
+    });
+  });
+
+  describe('when given a future date', () => {
+    it('returns true if past due ', async () => {
+      const dateToTestPlus91Days = dateToTest.getTime() + MS_PER_DAY * 91;
+      const futureDay = new Date(dateToTestPlus91Days);
+
+      const result = isLoanPastDue(futureDay, testLoan);
+
+      expect(result).to.eql(true);
+    });
+  });
+});
+
+describe('getLiquidatableLoans', () => {
+  describe('when given the current date', () => {
+    it('returns an array with loan IDs to liquidate', async () => {
+      const result = getLiquidatableLoans(
+        dateToTest,
+        loansTest,
+        etherTestPrice
+      );
+
+      const resultAwait = await result;
+
+      expect(resultAwait).to.eql([
+        6368,
+        6362,
+        6183,
+        6179,
+        6121,
+        6120,
+        6114,
+        5879
+      ]);
+    });
+  });
 });
